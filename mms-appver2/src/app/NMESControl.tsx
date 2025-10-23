d6e1676b2813ac6e79ab65bd1fd7df7e6bc898c2
sandrayoe <sandrayoe@gmail.com>
2025-10-23 18:08:54 +0200

    now move input parameters button

--- mms-appver2/src/app/NMESControl.tsx
+++ mms-appver2/src/app/NMESControl.tsx
@@ -520,7 +520,7 @@ const SensorPanel: React.FC = () => {
                     <input className={`${styles.textInput} ${styles.smallInput}`} value={motorPoints} onChange={(e) => { setMotorPoints(e.target.value); setParamsSubmitted(false); }} />
                   </label>
                 </div>
-                <div style={{ width: 180, display: 'flex', justifyContent: 'flex-end', alignItems: 'center', position: 'absolute', right: 10, top: '50%', transform: 'translateY(-50%)' }} className={styles.modifyAreaFixed}>
+                <div style={{ width: 180, display: 'flex', justifyContent: 'flex-end', alignItems: 'center', position: 'absolute', right: -40, top: '50%', transform: 'translateY(-50%)' }} className={styles.modifyAreaFixed}>
                   <button className={`${styles.button} ${styles.compactButton} ${styles.inputParamsNudge}`} onClick={handleApplyModify} disabled={!isConnected}>
                     Input Parameters
                   </button>
