d6bc2db83f6de5f4000e32b45a7bd4ca92d888a8
sandrayoe <sandrayoe@gmail.com>
2026-02-27 14:25:58 +0100

    fix BLE provider

--- mms-appver2/src/app/page.tsx
+++ mms-appver2/src/app/page.tsx
@@ -1,10 +1,5 @@
 import SensorPanel from './NMESControl';
-import { BluetoothProvider } from './BluetoothContext';
 
 export default function Home() {
-  return (
-    <BluetoothProvider>
-      <SensorPanel />
-    </BluetoothProvider>
-  );
+  return <SensorPanel />;
 }
--- mms-appver2/src/app/search/page.tsx
+++ mms-appver2/src/app/search/page.tsx
@@ -1,10 +1,5 @@
 import SearchAlgorithm from "./SearchAlgorithm";
-import { BluetoothProvider } from "../BluetoothContext";
 
 export default function SearchPage() {
-  return (
-    <BluetoothProvider>
-      <SearchAlgorithm />
-    </BluetoothProvider>
-  );
+  return <SearchAlgorithm />;
 }
